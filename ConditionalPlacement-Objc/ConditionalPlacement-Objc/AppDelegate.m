4dd52356ab999d211ea0738652db882cf97dc8d0
Justin Huntington <corusco@gmail.com>
2015-04-08 23:06:37 -0600

    Added project code

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -17,9 +17,30 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
+    //Declare two integers for the dudes
+    int karl = 10;
+    int leonard = 7;
     
+    //two more integers for place variables, employ ternary operators
+    int karlPlace = karl > 7 ? 1 : 2;
+    int leonardPlace = leonard > 7 ? 1 : 2;
     
+    NSLog(@"Karl's place is %d", karlPlace);
+    NSLog(@"Leonard's place is %d", leonardPlace);
+    
+    //Run the logic
+    if (karlPlace == 1 && leonardPlace == 1) {
+        NSLog(@"Both teachers are placed first.");
+    }
+    else if (karlPlace == 1) {
+        NSLog(@"Karl is placed first and Leonard is placed second.");
+    }
+    else if (leonardPlace == 1) {
+        NSLog(@"Leonard is placed first and Karl is placed second.");
+    }
+    else {
+        NSLog(@"Both teachers are placed second.");
+    }
     return YES;
 }
 
